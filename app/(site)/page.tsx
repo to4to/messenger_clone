5f61b50fcda8bd8b9a30b826450bb2d96dffb77a
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2023-05-30 21:47:35 +0530

    adding logo

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -4,6 +4,8 @@ export default function Home() {
     <div className="flex min-h-full flex-col justify-centre py-12 sm:px-6 lg:px-8 bg-gray-100">
       
 
+      <div className="sm:mx-auto sm:w-full sm:max-w-md"></div>
+
 
 
     
