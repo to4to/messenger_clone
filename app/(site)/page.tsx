c27f8a9c25391748040ac48d28550ea81d3ebe7e
Himanshu Kumar Singh <97310760+to4to@users.noreply.github.com>
2023-05-31 22:50:09 +0530

    starting with AuthForm

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -5,11 +5,14 @@ export default function Home() {
     <div className="flex min-h-full flex-col justify-centre py-12 sm:px-6 lg:px-8 bg-gray-100">
       
 
-      <div className="sm:mx-auto sm:w-full sm:max-w-md"></div>
+      <div className="sm:mx-auto sm:w-full sm:max-w-md">
 
       <Image src="/images/logo.png" alt='Logo'height="48" width="48" className="mx-auto w-auto"  />  
 
-      <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">Sign in To Your Account</h2>  
+      <h2 className="mt-6 text-center text-3xl font-bold tracking-tight text-gray-900">
+        Sign in To Your Account</h2>  
+</div>
+<AuthForm/>
       </div>
   );
 }
